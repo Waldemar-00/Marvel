5b79031f9bf4ae6ab44ad8fe2e2a9892796dc340
uladzimir <vova108vova@gmail.com>
2023-05-16 22:00:52 +0300

    structure changed

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -6,4 +6,7 @@ class MarvelServices {
     }
     return await result.json()
   }
+  getAllCharacters = () => {
+    return this.getService('https://gateway.marvel.com:443/v1/public/characters?apikey=f1a00c9aefc1b72256ac8e8a4fb5af16')
+  }
 }
