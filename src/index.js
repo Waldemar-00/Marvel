563b260688b0e9298486417deb32fb6ed2999adc
uladzimir <vova108vova@gmail.com>
2023-05-17 20:27:59 +0300

    getting data from resource marvel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/app/App';
-import './style/style.scss';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/app/App'
+import './style/style.scss'
+import MarvelServices from './services/MarvelService'
+const characters = new MarvelServices()
+characters.getAllCharacters().then(result => console.log(result))
+characters.getAllCharacters().then(result => {
+  result.data.results.forEach(object => {
+    console.log(object.name)
+  })
+})
+characters.getOneCharacter(1011052).then(result => console.log(result))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,7 @@
+//! clean JS
 class MarvelServices {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/'
+  _apiRey = 'apikey=f1a00c9aefc1b72256ac8e8a4fb5af16'
   getService = async (url) => {
     const result = await fetch(url)
     if (!result.ok) {
@@ -7,6 +10,10 @@ class MarvelServices {
     return await result.json()
   }
   getAllCharacters = () => {
-    return this.getService('https://gateway.marvel.com:443/v1/public/characters?apikey=f1a00c9aefc1b72256ac8e8a4fb5af16')
+    return this.getService(`${this._apiBase}characters?limit=9&offset=210&${this._apiRey}`)
   }
+  getOneCharacter = (character) => {
+  return this.getService(`${this._apiBase}characters/${character}?${this._apiRey}`)
 }
+}
+export default MarvelServices
