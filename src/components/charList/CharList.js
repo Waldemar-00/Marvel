b86a4da0c697b18d46a28743baa9165823d43786
uladzimir <vova108vova@gmail.com>
2023-05-23 21:00:43 +0300

    added nine characters to the page from the Marvel server

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,4 @@
 import './charList.scss'
-import abyss from '../../resources/img/abyss.jpg'
 import { Component } from 'react'
 import Spinner from '../spinner/Spinner'
 import ErrorMessage from '../error/ErrorMessage'
@@ -7,54 +6,53 @@ import MarvelServices from '../../services/MarvelService'
 class CharList extends Component {
     state = {
         arrayOfCharacters: [],
-        spinner: true,
+        loading: true,
         error: false,
     }
     characters = new MarvelServices()
     componentDidMount() {
-        this.characters.getAllCharacters().then(result => console.log(result)).catch(error => console.log("409-409"))
+        this.characters.getAllCharacters().then(result => {
+            console.log(result)
+            return this.onLoad(result)
+        }).catch(error => console.log(this.onError))
+    }
+    onLoad = (result) => {
+        this.setState({
+            arrayOfCharacters: result,
+            loading: false
+        })
+    }
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+        
+    }
+    makeLi = (array) => {
+        const list = array.map(li => {
+            const liStyle = li.thumbnail.includes('image_not_available') ? { 'objectFit': 'fill' } : { 'objectFit': 'cover' }
+            return (
+                <li className="char__item" key={li.id}>
+                    <img src={li.thumbnail} alt={li.name} style={liStyle}/>
+                    <div className="char__name">{li.name}</div>
+                </li>
+            )
+        })
+        return (
+            <ul className="char__grid">
+                {list}
+            </ul>
+        )
     }
     render() {
+        const { arrayOfCharacters, loading, error } = this.state
+        const errorMessage = error ? <ErrorMessage /> : null
+        const spinner = loading ? <Spinner /> : null
+        const contant = error ? errorMessage : loading ? spinner : this.makeLi(arrayOfCharacters)
         return (
             <div className="char__list">
-                <ul className="char__grid">
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item char__item_selected">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src={abyss} alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                </ul>
+                {contant}
                 <button className="button button__main button__long">
                     <div className="inner">load more</div>
                 </button>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,7 +24,8 @@ class MarvelServices {
       description: character.description,
       thumbnail: character.thumbnail.path + '.' + character.thumbnail.extension,
       homepage: character.urls[0].url,
-      wiki: character.urls[1].url
+      wiki: character.urls[1].url,
+      id: character.id
     }
   }
 }
