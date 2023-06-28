ba6fe26ca092c06fc803e77af29a9a730668be00
uladzimir <vova108vova@gmail.com>
2023-06-28 20:45:06 +0300

    activated components: AppBanner, ComicsList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,34 +1,38 @@
 import AppHeader from "../appHeader/AppHeader"
-import RandomChar from "../randomChar/RandomChar"
+// import RandomChar from "../randomChar/RandomChar"
 import CharList from "../charList/CharList"
-import CharInfo from "../charInfo/CharInfo"
-import { useState } from "react"
-import ErrorBoundary from "../errorBoundary/ErrorBoundary"
+// import CharInfo from "../charInfo/CharInfo"
+// import { useState } from "react"
+// import ErrorBoundary from "../errorBoundary/ErrorBoundary"
 import { PropTypes } from "prop-types"
+import AppBanner from "../appBanner/AppBanner"
+import ComicsList from "../comicsList/ComicsList"
 
-import decoration from '../../resources/img/vision.png';
+// import decoration from '../../resources/img/vision.png'
 
 const App = () => {
-    const [character, setCharacter] = useState(null)
-    const onSelectedCharacter = (id) => {
-        setCharacter(id)
-    }
+    // const [character, setCharacter] = useState(null)
+    // const onSelectedCharacter = (id) => {
+        // setCharacter(id)
+    // }
         return (
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <ErrorBoundary>
-                        <RandomChar />
-                    </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList upStateForCharacter={onSelectedCharacter} />
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                            <CharInfo getIdCharacterFromState={character} />
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                  {/* <ErrorBoundary> */}
+                      {/* <RandomChar /> */}
+                  {/* </ErrorBoundary> */}
+                  {/* <div className="char__content"> */}
+                      {/* <ErrorBoundary> */}
+                          {/* <CharList upStateForCharacter={onSelectedCharacter} /> */}
+                      {/* </ErrorBoundary> */}
+                      {/* <ErrorBoundary> */}
+                          {/* <CharInfo getIdCharacterFromState={character} /> */}
+                      {/* </ErrorBoundary> */}
+                  {/* </div> */}
+                  {/* <img className="bg-decoration" src={decoration} alt="vision" /> */}
+                <AppBanner />
+                <ComicsList/>
                 </main>
             </div>
         )
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,9 +2,13 @@
 //https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=bd8673ecbff251cdb59ed3a0d89b43e2
 //https://gateway.marvel.com:443/v1/public/characters/110456?apikey=bd8673ecbff251cdb59ed3a0d89b43e2
 class MarvelServices { 
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/characters'
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/'
   _apiKey = 'apikey=bd8673ecbff251cdb59ed3a0d89b43e2'
   _baseOffset = 210
+  getAllComics = async (offset = 0) => {
+		const result = await this.getResource(`${this._apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${this._apiKey}`)
+		return result.data.results.map(this._transformComics)
+	}
   getResource = async (url) => {
     const result = await fetch(url)
     if (!result.ok) {
@@ -13,11 +17,11 @@ class MarvelServices {
     return await result.json()
   }
   getAllCharacters = async (offset = this._baseOffset) => {
-    const result = await this.getResource(`${this._apiBase}?limit=9&offset=${offset}&${this._apiKey}`)
+    const result = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`)
     return result.data.results.map(this._transformCharacter)
   }
   getOneCharacter = async (characterId) => {
-    const result = await this.getResource(`${this._apiBase}/${characterId}?${this._apiKey}`)
+    const result = await this.getResource(`${this._apiBase}characters/${characterId}?${this._apiKey}`)
     return this._transformCharacter(result.data.results[0])
   }
   _transformCharacter = (character) => {
@@ -31,5 +35,20 @@ class MarvelServices {
       comics: character.comics.items
     }
   }
+  _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		}
+	}
 }
 export default MarvelServices
